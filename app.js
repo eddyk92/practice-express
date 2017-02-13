ff871866e7e92297e196504cf5f390e9579f1a9c
Kevin Eddye <kevneddy@gmail.com>
2017-02-13 13:42:26 -0800

    Add server and routes

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+// var path = require('path');
+var app = express();
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+var server = app.listen(5000, function(){
+	console.log('server running on port 5000!')
+});
